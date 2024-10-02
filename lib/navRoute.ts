09c4f6ba107444af07d67cd49eac9338ce22bbd5
hcoretech <get2henry14@gmail.com>
2024-10-02 03:49:08 -0700

    new change coming

---
+++ lib/navRoute.ts
@@ -0,0 +1,38 @@
+'use client'
+
+import { MdOutlineAddHomeWork } from "react-icons/md";
+import { CiChat1 } from "react-icons/ci";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+// import { type } from "os";
+
+// import { usePathname } from "next/navigation";
+
+// const pathname = usePathname;
+// "let ho = </>"
+
+export const navLink = [
+
+    {
+        label:"Home",
+        route:"/home",
+        icon: "/download.svg"
+        
+    },
+    {
+        label:"Search",
+        route:"search",
+        icon:"/download.svg"
+    },
+    {
+        label:"Downloads",
+        route:"/download",
+        icon:"/download.svg"
+    },
+    {
+        label:"Community",
+        route:"Chat",
+        icon:"/download.svg"
+
+    }
+]
