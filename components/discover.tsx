357ffab43d4e3110edd80bc7fc7be3c462430455
unknown <get2henry14@gmail.com>
2024-10-21 07:17:28 +0100

    good morning from this side

--- components/discover.tsx
+++ components/discover.tsx
@@ -4,7 +4,7 @@ import { CarouselPlugin } from "./carousel";
 export const Discovers = async() => {
     return(
       <section className="w-full">
-        <header className="  flex items-center gap-2">
+        <header className="  flex items-center gap-2 p-2 shadow-md shadow-green-200 ">
           <div className="p-2 bg-[#05140C] rounded-full"> 
             <Image className="w-[25px] h-[25px]" width={50} height={50} src='/main.svg' alt="logo"/>
             </div>
