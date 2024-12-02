f973012f6723bffdb1842beb91fec63e1671c9a9
unknown <get2henry14@gmail.com>
2024-12-02 21:22:45 +0100

    new login change

--- app/api/loginRoute/route.ts
+++ app/api/loginRoute/route.ts
@@ -1,22 +1,16 @@
 'use server'
 import { NextResponse } from "next/server";
-// const jwt = require('jsonwebtoken');
+
 import { User } from "../../../lib/userModel";
 import { cookies } from "next/headers";
-// import dbConnection from "../../../lib/mongodb";
+
 import { nanoid } from "nanoid";
 import { SignJWT } from "jose";
-// import { jwtCheck } from "../../../lib/constant";
-import { jwtSecret } from "../../../lib/constant";
-// export const dynamic ='force-dynamic'
+
 import client from "../../../lib/mongodb";
 
-// const createToken = (_id) => {
-//     return jwt.sign({_id}, process.env.my_SECRET, { expiresIn: "1d" })
-//   }
-  
-export async function POST(req:Request){
-    // await dbConnection();
+export async function POST(req:Request):Promise<NextResponse>{
+
     const clientd = client;
     const db =  clientd.db('software_city');
     const user = db.collection('users')
@@ -24,16 +18,21 @@ export async function POST(req:Request){
     const {email,password} = body
 
     try{
+
+        if(email.indexOf("@")== -1){
+         throw Error ("invalid email address ");
+        }
+      
         const finds = await user.findOne({
           'email':email,
           'password':password         
         })
 
-        if(!finds){
-            return NextResponse.json({message:"no user found with such id try again"},
-             {status:400}
-            )
-        }
+          if(!finds){
+            throw Error("no user found check info and try again");
+            
+          }
+       
         const id = finds._id.toString();
         
         const token = await new SignJWT({})
@@ -41,7 +40,7 @@ export async function POST(req:Request){
           .setJti(nanoid())
           .setIssuedAt()
           .setExpirationTime('1day')
-          .sign(new TextEncoder().encode(jwtSecret));
+          .sign(new TextEncoder().encode(process.env.my_SECRET));
 
             (await cookies()).set('userId', id, {
             httpOnly: true,
@@ -59,11 +58,12 @@ export async function POST(req:Request){
          maxAge: 60 * 60 * 24, 
        });
       
-          return NextResponse.json({message:'logged succesfull'},{status:200})         
+          return NextResponse.json({message:'logged succesfull',status:200})  
+          // return NextResponse.redirect('/Home',{status:200})       
 
     }
     catch(error){
-        return NextResponse.json({error:'Error in sign-in,check internet connection'},{status:400})
+        return NextResponse.json({message:error},{status:400})
      }
-
+      
     }
