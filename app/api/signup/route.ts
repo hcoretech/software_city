a873a294b71c176d08089b93aa2917d16f998e0c
unknown <get2henry14@gmail.com>
2024-12-03 04:08:41 +0100

    new sign in

---
+++ app/api/signup/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import client from "../../../lib/mongodb";
+
+
+export default async function POST(request:NextRequest){
+
+    const body = await request.json();
+    const userData = await body;
+    try {
+        if(!userData){
+            throw "no data found in the request body";
+        }
+        const db = client.db('software_city');
+        const dbCreate =  db.collection('soundXusers');
+
+          console.log('working with try') ;      
+         const createAccount = await dbCreate.insertOne({userData});
+         console.log("working")
+         const response = createAccount;
+        
+         if(!response){
+           throw  "user creation failed";
+         }
+         return NextResponse.json({response},{status:200});
+
+        
+    } catch (error) {
+       return NextResponse.json({message:error},{status:400});
+    }
+
+}
